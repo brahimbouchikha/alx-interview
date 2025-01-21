346f2694403bc40f78867eb486780bf3e999ae6a
brahim <brahim@localhost.localdomain>
2025-01-21 01:06:12 +0100

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,18 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api/';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+const printCharacterNames = (characters, index) => {
+  if (index === characters.length) return;
+  request(characters[index], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    printCharacterNames(characters, index + 1);
   });
-}
+};
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const characters = JSON.parse(body).characters;
+  printCharacterNames(characters, 0);
+});
